3b7de51eb3c5d386e6bc86ef29ed41ade502007f
Vivek Kiran Ballakur <thevivekkiran@gmail.com>
2024-03-22 22:53:19 -0500

    Add apptainer to path

--- nwchem-dev-lawrence/install-apptainer-tools.sh
+++ nwchem-dev-lawrence/install-apptainer-tools.sh
@@ -13,6 +13,9 @@ curl -s https://raw.githubusercontent.com/apptainer/apptainer/main/tools/install
 
 export APPTAINER_CACHEDIR="$HOME/.apptainer/cache"
 export APPTAINER_SINGULARITY_PATH="$HOME/apptainer-tools"
+export APPTAINER_BIN="$HOME/apptainer-tools/bin:$APPTAINER_SINGULARITY_BIN $HOME/apptainer-tools/bin/apptainer"
+export SINGULARITY_BIN="$HOME/apptainer-tools/bin:$APPTAINER_SINGULARITY_BIN $HOME/apptainer-tools/bin/singularity"
+
 export PATH="$PATH:$APPTAINER_SINGULARITY_PATH/apptainer-tools/bin"
 
 source ~/.bashrc
