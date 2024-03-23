abbdd21d3abf59d25b3dd0f3128a653a4792bcf9
Vivek Kiran Ballakur <thevivekkiran@gmail.com>
2024-03-22 21:50:38 -0500

    apptainer-tools.sh

--- nwchem-dev-lawrence/install-apptainer-tools.sh
+++ nwchem-dev-lawrence/install-apptainer-tools.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 curl -s https://raw.githubusercontent.com/apptainer/apptainer/main/tools/install-unprivileged.sh | \
-    bash -s - apptainer-tools
+    bash -s - $HOME/apptainer-tools
 
 
 ./apptainer-tools/bin/apptainer  pull -F --name ./nwchems_`id -u`.img oras://ghcr.io/edoapra/nwchem-singularity/nwchem-dev.ompi41x:latest
