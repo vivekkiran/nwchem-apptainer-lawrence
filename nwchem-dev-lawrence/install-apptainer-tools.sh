8fa5b5b522d8cd2ba5e980bb2724d854e3f3ad65
Vivek Kiran Ballakur <thevivekkiran@gmail.com>
2024-03-22 21:43:07 -0500

    apptainer-tools.sh

--- nwchem-dev-lawrence/install-apptainer-tools.sh
+++ nwchem-dev-lawrence/install-apptainer-tools.sh
@@ -6,6 +6,7 @@ curl -s https://raw.githubusercontent.com/apptainer/apptainer/main/tools/install
 
 ./apptainer-tools/bin/apptainer  pull -F --name ./nwchems_`id -u`.img oras://ghcr.io/edoapra/nwchem-singularity/nwchem-dev.ompi41x:latest
 
+rm -r "$HOME/apptainer-tools/bin"
 export APPTAINER_CACHEDIR="$HOME/.apptainer/cache"
 export APPTAINER_SINGULARITY_PATH="$HOME/apptainer-tools/bin"
 
